6ea89f7c361b97ca86511717c290a62979994294
Mahmoud Fathy <mahmoud.fathy1@vodafone.com>
2024-06-15 16:21:45 +0300

    ss

--- redirect_google_to_youtube.py
+++ redirect_google_to_youtube.py
@@ -3,4 +3,6 @@ from mitmproxy import http
 def request(flow: http.HTTPFlow) -> None:
     if "google.com" in flow.request.pretty_host:
         flow.request.host = "www.youtube.com"
+        flow.request.port = 443 if flow.request.scheme == "https" else 80
         flow.request.path = "/"
+        flow.request.scheme = "https"  # Assuming you want to redirect to HTTPS YouTube
